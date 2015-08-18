ef1f9e94c0149194c9a345deb9284ceea4eb569b
Jian Weihang <tonytonyjan@gmail.com>
2015-08-18 19:27:04 +0800

    update models

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,4 +2,8 @@ class LineItem < ActiveRecord::Base
   belongs_to :order
   belongs_to :cart
   belongs_to :product
+
+  def subtotal
+    quantity * unit_price
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ActiveRecord::Base
   has_many :line_items
   has_many :products, through: :line_items
+
+  def total
+    line_items.to_a.sum(&:subtotal)
+  end
 end
