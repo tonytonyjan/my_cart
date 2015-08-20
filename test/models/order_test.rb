d8f0284b8ecaa0f525f276fbd8c8db3484f2ae67
Jian Weihang <tonytonyjan@gmail.com>
2015-08-20 22:43:03 +0800

    add test

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
+  test '::new_from_cart' do
+    order = Order.new_from_cart carts(:one)
+    assert_equal carts(:one).line_item_ids.sort, order.line_item_ids.sort
+  end
+
   test '#total' do
     assert_equal 3250, carts(:one).total
   end
