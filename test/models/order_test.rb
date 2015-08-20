25f262e2aff52082aa319e61b9e44deb748a0ed3
Jian Weihang <tonytonyjan@gmail.com>
2015-08-20 21:44:36 +0800

    add order, trade test

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -4,4 +4,9 @@ class OrderTest < ActiveSupport::TestCase
   test '#total' do
     assert_equal 3250, carts(:one).total
   end
+
+  test '#paid?' do
+    assert orders(:paid_order).paid?
+    refute orders(:unpaid_order).paid?
+  end
 end
